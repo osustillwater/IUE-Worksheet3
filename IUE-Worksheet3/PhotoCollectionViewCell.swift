195a9c7a773ed4518909d667c797a1a497836646
Mohammad Saiful Kabir <2190269@c2-lcm-m08.local>
2019-10-09 15:21:37 +0100

    Added file

---
+++ IUE-Worksheet3/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  IUE-Worksheet3
+//
+//  Created by Mohammad Saiful Kabir on 09/10/2019.
+//  Copyright © 2019 Mohammad Saiful Kabir. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: CollectionViewCell {
+
+}
